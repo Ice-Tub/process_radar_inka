3d4d35c50bab16ba191a6d14d495a6012c50074a
inkakoch <inka.koch@uni-tuebingen.de>
2022-02-17 14:45:00 +0100

    made section headings

--- Main_layer_firn_correct.m
+++ Main_layer_firn_correct.m
@@ -1,14 +1,12 @@
 clear all
 
-% this fuction needs Antarctic mapping tools installed/downloaded
+%% add paths to functions and data sets on the server/local
 % Antarctic Mapping Tools: https://www.mathworks.com/matlabcentral/fileexchange/47638
-addpath('Z:\docs\ikoch\src\AntarcticMappingTools'); % change this to your path
+addpath('Z:\docs\ikoch\src\AntarcticMappingTools'); 
 addpath('Z:\docs\ikoch\src\AMT');
-% and Antarctic Bedmachine codes
-% https://github.com/chadagreene/BedMachine
-addpath('Z:\docs\ikoch\src\AMT\BedMachine'); % change this to your path and also change the path within codes to access the following data file
-% and data: https://nsidc.org/data/nsidc-0756 
-%add final fuctions
+addpath('Z:\docs\ikoch\src\AMT\BedMachine'); %https://github.com/chadagreene/BedMachine
+
+% these paths need to be updated to local (after downloading from GitHub)
 addpath('D:\Publications\Koch_ice_shelf_characteristics\src\process_radar');
 addpath('D:\Publications\Koch_ice_shelf_characteristics\src\process_radar\auxfunctions');
 addpath('D:\Publications\Koch_ice_shelf_characteristics\src\process_radar\auxfunctions\Bedmachine');
@@ -16,7 +14,16 @@ addpath('D:\Publications\Koch_ice_shelf_characteristics\src\process_radar\auxfun
 %add path to Geoid, FA file, Antarctica REMA
 Geoidpath = 'Z:\data\ikoch\data_large\Data_general_Antarctica\EIGEN-6C4\geoid_large_domain';
 FApath = 'Z:\data\ikoch\data_large\Data_general_Antarctica\Bedmachine_FA_content\firn22.tif';
-addpath('Z:\data\ikoch\data_large\BedMachine'); %add path to 'BedMachineAntarctica_2020-07-15_v02.nc' 
+addpath('Z:\data\ikoch\data_large\BedMachine'); %add path to 'BedMachineAntarctica_2020-07-15_v02.nc' download data from: https://nsidc.org/data/nsidc-0756 
+
+%% set variables for density correction
+MaxDepth=1000; %in metres
+dz=0.001; % in meters.
+%rho = 910-460*exp(-0.033*z);%for RBIS
+rho = 910-460*exp(-0.025*z);%for DIR 
+z=0:dz:MaxDepth;
+
+%% load layer data in open folder
 
 myDir = cd; %folder with data to be processed, needs to be open as 'Current Folder'
 myFiles = dir(fullfile(myDir,'*.mat')); %gets all mat files in struct
@@ -32,14 +39,8 @@ Data.(my_field) = importdata(FileName_org);
 %GeoidFirnFileName = ['D:\Radar\Data\AWI_flights\Data\Picked_data\processed\Firncorrect_Bedmach_surf\GeoidFirn' FileName '.csv'];
 end
 
-%%set variables for density correction
-MaxDepth=1000; %in metres
-dz=0.001; % in meters.
-%rho = 910-460*exp(-0.033*z);%for RBIS
-z=0:dz:MaxDepth;
-rho = 910-460*exp(-0.025*z);%for DIR 
+%% concacenate layers together
 
-%concacenate layers together
 %FIX: create loop to concacenate and also adjust for variable number of data files
 Dataall.psX = [Data.v1.psX Data.v2.psX Data.v3.psX Data.v4.psX Data.v5.psX Data.v6.psX Data.v7.psX Data.v8.psX Data.v9.psX];
 Dataall.psY = [Data.v1.psY Data.v2.psY Data.v3.psY Data.v4.psY Data.v5.psY Data.v6.psY Data.v7.psY Data.v8.psY Data.v9.psY];
@@ -50,8 +51,7 @@ dt=Dataall.twt(3)-Dataall.twt(2);
 [nr nc]=size(Data.v1.layers_relto_surface);
 
 %min horizons that overlap - check why only 8
-%min_nr=8;%usually would use nr
-min_nr=1;%usually would use nr
+min_nr=8;%usually would use nr, something needs to be fixed with final layer files
 
 %%change hardcoding!
 for kk=1:min_nr
@@ -66,8 +66,9 @@ hold on
 set(gca,'Zdir','reverse')
 end
 
-nr=1; %only added for test purposes
+%% run firncorrection
 
+nr=1; %only added for test purposes
 %Firncorrection (external function)
 %It is taking lots of time!!!
 Dataall=Firncorrect(Dataall, rho, dt, dz, MaxDepth, nr, nc,z);
@@ -80,12 +81,10 @@ hold on
 set(gca,'Zdir','reverse')
 end
 
+%% surface Topo correction
+
 Dataall=ExtractElevationFromGeiod(Dataall,Geoidpath); 
 Dataall=ExtractFAcontent(Dataall,FApath);
-%the following uses the fuction bedmachine_data, which accesses the AMT toolbox and the
-%AntarcticaFilename = 'BedMachineAntarctica_2020-07-15_v02.nc'; 
-%Bedmachine surface is using the REMA DEM smoothly interpolated
-addpath('Z:\docs\ikoch\src\AntarcticMappingTools'); 
 Dataall.Surface_Bedmachine_ice = bedmachine_interp('surface',Dataall.psX,Dataall.psY);
 Dataall.Surface_Bedmachine_firn = Dataall.Surface_Bedmachine_ice+Dataall.FA;
 Dataall.Surface_REMA_fromBedmachine_firn=Dataall.Surface_Bedmachine_ice+Dataall.geoid+Dataall.FA;
@@ -108,7 +107,12 @@ xlabel('Eastings')
 ylabel('Northings')
 zlabel('Elevation (m)')
 
+%% save data
+
 Geoall_FirnFileName = ['D:\Publications\Koch_ice_shelf_characteristics\Data\Picked_Layers\Geoall_' FileName];
+save(Geoall_FirnFileName,'-struct', 'Dataall')
+
+%% correct bed pick, consider Bedmachine ice and average FA for compatability with Elmer ice
 
 %%NEED to fix and add the following for depth conversion of the basal pick
 %%and co-plotting with ELMER ice surfaces considering ice and FA
@@ -154,8 +158,6 @@ Geoall_FirnFileName = ['D:\Publications\Koch_ice_shelf_characteristics\Data\Pick
 % Data.elevation_bed_Bedmachine_ice=Data.Surface_Bedmachine_ice-(Data.depth_bed-FA_layer222);
 % Data.elevation_bed_REMA_fromBedmachine_firn=Data.Surface_REMA_fromBedmachine_firn-Data.depth_bed;
 % Data.elevation_bed_geoid_firn=Data.Surface_geoid_firn-Data.depth_bed;
-
-save(Geoall_FirnFileName,'-struct', 'Dataall')
 
 %need to combine for all layers 
 % kk1=1;
